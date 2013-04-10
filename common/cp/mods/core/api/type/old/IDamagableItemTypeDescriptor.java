0d49c53aba288aecfde411643d962c2943ffeadd
Myrddraall <Erd@Dark-Nexus>
2013-04-10 18:00:24 -0400

    Cleanup

--- common/cp/mods/core/api/type/old/IDamagableItemTypeDescriptor.java
+++
@@ -1,6 +0,0 @@
-package cp.mods.core.api.type.old;
-
-public interface IDamagableItemTypeDescriptor extends IItemTypeDescriptor
-{
-    Enum<? extends IDamagableItemTypeDescriptor> getRelatedType();
-}
--- common/cp/mods/core/api/type/old/ISimpleItemTypeDescriptor.java
+++
@@ -1,8 +0,0 @@
-package cp.mods.core.api.type.old;
-
-import net.minecraft.item.Item;
-
-public interface ISimpleItemTypeDescriptor extends IItemTypeDescriptor
-{
-    public void initialize(Class<? extends Item> item);
-}
